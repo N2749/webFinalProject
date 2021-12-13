03d280bb2568d6d348e3aea1cc575923265ee12c
Nurbolat Davletyarov <nurba.dav@gmail.com>
2021-12-14 00:21:38 +0600

    login feature was added + naming fixed again

--- login.js
+++ login.js
@@ -1,45 +1,25 @@
 const form = document.getElementById("form");
-const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
-var correctCounter;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    correctCounter = 0;
-
     checkInputs();
-    if(correctCounter == 4) {
-        addUser();
-        success();
-    }
-    
+    userExistence();
 });
 
 function checkInputs() {
     //trim is used to remove spaces
-    const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-    if (usernameValue === "") {
-        setErrorFor(username, "Username cannot be blank");
-    } else if (!isUsername(usernameValue)) {
-        setErrorFor(username, "Not a valid username");
-    } else if (localStorage.getItem(emailValue)) {
-        setErrorFor(username, "Username is taken");
-    } else {
-        setSuccessFor(username);
-    }
 
     if (emailValue === "") {
         setErrorFor(email, "Email cannot be blank");
     } else if (!isEmail(emailValue)) {
         setErrorFor(email, "Not a valid email");
-    } else if (localStorage.getItem(emailValue)) {
-        setErrorFor(email, "Email is taken");
+    } else if (!localStorage.getItem(emailValue)) {
+        setErrorFor(email, "User does not exist");
     } else {
         setSuccessFor(email);
     }
@@ -56,14 +36,6 @@ function checkInputs() {
     } else {
         setSuccessFor(password);
     }
-
-    if (password2Value === "") {
-        setErrorFor(password2, "Password2 cannot be blank");
-    } else if (passwordValue !== password2Value) {
-        setErrorFor(password2, "Passwords does not match");
-    } else {
-        setSuccessFor(password2);
-    }
 }
 
 function setErrorFor(input, message) {
@@ -76,11 +48,6 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
-    correctCounter++;
-}
-
-function isUsername(username) {
-    return /^[a-zA-Z0-9]+$/.test(username);
 }
 
 function isEmail(email) {
@@ -99,17 +66,18 @@ function isPassword(password) {
     );
 }
 
-function addUser() {
-    localStorage.setItem(email.value.trim(), JSON.stringify({
-        username: username.value.trim(),
-        email: email.value.trim(),
-        password: password.value.trim(),
-        block: false
-    }))
+function userExistence() {
+    if (localStorage.getItem(email.value.trim())) {
+        let user = JSON.parse(localStorage.getItem(email.value.trim()));
+        if (user.password === password.value.trim()) 
+            success();
+        else 
+            setErrorFor(password, "Incorrect Password");
+        return true;
+    }
+    setErrorFor(password, "Type email first");
 }
 
 function success() {
-    let choice = confirm("You have succesfully registered. do you want to leave registration page?");
-    if(choice)
-    document.location.href = "start.html";
+    document.location.href = "mainpage.html";
 }
